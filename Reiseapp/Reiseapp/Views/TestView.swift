a7472428738dfc6f3b54f90fcde624c8fe39690e
BenjaminVodel <Benni.997@web.de>
2025-09-08 11:18:56 +0200

    ViewsOrder

---
+++ Reiseapp/Reiseapp/Views/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Reiseapp
+//
+//  Created by Benjamin Vodel on 08.09.25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
