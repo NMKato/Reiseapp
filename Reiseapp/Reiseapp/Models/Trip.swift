03f412923fe6ca284fcf255aad6a6f0826a61a3a
Waldemardietler <wdietler@googlemail.com>
2025-09-08 11:37:56 +0200

    Models angelegt

---
+++ Reiseapp/Reiseapp/Models/Trip.swift
@@ -0,0 +1,19 @@
+//
+//  Trip.swift
+//  Reiseapp
+//
+//  Created by Waldemar Dietler on 08.09.25.
+//
+
+import SwiftUI
+
+struct Trip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+#Preview {
+    Trip()
+}
