4ea44f71aa7a502c988f277e646b4632d2bcc74e
FloricaGirisci <florica.girisci@yahoo.com>
2025-09-08 15:18:46 +0200

    AddtRIPvIEWs

---
+++ Reiseapp/Reiseapp/ViewModel/AddTripViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AddTripViewModel.swift
+//  Reiseapp
+//
+//  Created by Florica Girisci on 08.09.25.
+//
+
+import SwiftUI
+
+struct AddTripViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTripViewModel()
+}
---
+++ Reiseapp/Reiseapp/Views/AddTripView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTripView.swift
+//  Reiseapp
+//
+//  Created by Florica Girisci on 08.09.25.
+//
+
+import SwiftUI
+
+struct AddTripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTripView()
+}
